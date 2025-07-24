66fdabed459710e370732d8809aff1c877444330
monok8i <didkivskiyyura725@gmail.com>
2025-07-24 10:25:11 +0200

    fix: add another location of cliphist window

--- scripts/clipboard-history.sh
+++ scripts/clipboard-history.sh
@@ -6,8 +6,10 @@ display=$(echo "$original" | sed 's/^[0-9]\+\t//' | sed 's/\[\[ binary data \([0
 IFS=$'\n' read -d '' -r -a orig_lines <<< "$original"
 IFS=$'\n' read -d '' -r -a disp_lines <<< "$display"
 
+ROFI_OVERRIDE="window{location:center;anchor:north;}"
+
 # Показуємо в rofi та отримуємо індекс
-selected_index=$(printf '%s\n' "${disp_lines[@]}" | rofi -dmenu -theme ~/.config/rofi/dmenu/config.rasi -p Cliphist: -format i)
+selected_index=$(printf '%s\n' "${disp_lines[@]}" | rofi -dmenu -theme ~/.config/rofi/dmenu/config.rasi -theme-str "${ROFI_OVERRIDE}" -p Cliphist: -format i)
 
 if [ -n "$selected_index" ]; then
     echo "${orig_lines[$selected_index]}" | cliphist decode | wl-copy
